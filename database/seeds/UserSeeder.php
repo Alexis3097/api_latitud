53a6b6dcf1915cd0e3037f88038f5b459d275383
Alexis3097 <alexis.montoya97@hotmail.com>
2021-06-28 16:01:44 -0500

    cambiando correo

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
         User::create([
             'user_type_id'=> 2,
             'name'=> 'cristian tecnico',
-            'email'=> 'admin@gmail.com',
+            'email'=> 'cristian@gmail.com',
             'password'=> Hash::make('adminadmin'),
         ]);
     }
