6cbb7a55e583acc129488f5432323c4aae1e1066
Alexis3097 <alexis.montoya97@hotmail.com>
2021-07-09 12:43:54 -0500

    prueba de obtener box

--- app/Repositories/CashRegisterRepository.php
+++ app/Repositories/CashRegisterRepository.php
@@ -6,14 +6,18 @@ namespace App\Repositories;
 
 use App\IRepositories\ICashRegisterRepository;
 use App\Models\CashRegister;
+use Illuminate\Database\Eloquent\Builder;
 
 class CashRegisterRepository implements ICashRegisterRepository
 {
 
     public function all()
     {
-        return CashRegister::all();
-//        return CashRegister::with('box');
+        return CashRegister::with('box')->get();
+
+//        $register = CashRegister::whereHas('box', function (Builder $query) {
+//            $query->where('content', 'like', 'foo%');
+//        })->get();
     }
 
     public function create($data)
