878b2007a3b332bd445563d81b455c7e9aaac4e9
Alexis3097 <alexis.montoya97@hotmail.com>
2021-07-12 10:57:18 -0500

    errors 3

--- app/Http/Controllers/CashRegisterController.php
+++ app/Http/Controllers/CashRegisterController.php
@@ -32,17 +32,17 @@ class CashRegisterController extends Controller
         }
     }
     function registersCajaChia(){
-        try{
+//        try{
             $cashRegister = $this->ICashRegisterRepository->registersCajaChia();
-            if(!is_null($cashRegister)){
+//            if(!is_null($cashRegister)){
                 return CashRegisterResource::collection($cashRegister);
-            }
-            else{
-                return response()->json(['messages'=> ResponseMessages::GET_RESOURCES_VOID()]);
-            }
-        }catch (Throwable $e){
-            Log::info(ResponseMessages::GET_RESOURCES_FAILED_500() .$e);
-            return response()->json(['get'=>$e],500);
-        }
+//            }
+//            else{
+//                return response()->json(['messages'=> ResponseMessages::GET_RESOURCES_VOID()]);
+//            }
+//        }catch (Throwable $e){
+//            Log::info(ResponseMessages::GET_RESOURCES_FAILED_500() .$e);
+//            return response()->json(['get'=>$e],500);
+//        }
     }
 }
