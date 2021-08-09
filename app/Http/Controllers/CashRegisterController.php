0c8e34637ff9377ed0d83ab3f17513da5cf683a0
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-09 13:57:51 -0500

    fix 4

--- app/Http/Controllers/CashRegisterController.php
+++ app/Http/Controllers/CashRegisterController.php
@@ -47,9 +47,9 @@ class CashRegisterController extends Controller
         }
     }
 
-    function show(Request $request){
+    function show($id){
         try{
-            $detailRegisters = $this->ICashRegisterRepository->getDetailRegister($request->id);
+            $detailRegisters = $this->ICashRegisterRepository->getDetailRegister($id);
             if(!is_null($detailRegisters)){
                 return new CashRegisterResource($detailRegisters);
             }
