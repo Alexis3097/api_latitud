31034db80df36a4072854eebb5f5853f25a3b3db
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-16 19:55:38 -0500

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
 //            }
         }catch (Throwable $e){
             Log::info(ResponseMessages::STORE_FAILED_500().$e);
-            return response()->json(['errorCatch'=>$user]);
+            return response()->json(['errorCatch'=>$e]);
         }
     }
 
