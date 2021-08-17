5ce2dc6f4d072336fd1a3bc614f5f9ecfd1af3a2
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-16 19:23:28 -0500

    viendo problema del store

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,7 +44,7 @@ class UserController extends Controller
             }
         }catch(Throwable $e){
             Log::info(ResponseMessages::GET_RESOURCES_FAILED_500() .$e);
-            return response()->json($e);
+            return response()->json(['get'=>false],500);
         }
     }
 
@@ -58,7 +58,7 @@ class UserController extends Controller
             }
         }catch (Throwable $e){
             Log::info(ResponseMessages::STORE_FAILED_500().$e);
-            return response()->json(['store'=>false],500);
+            return response()->json($e);
         }
     }
 
