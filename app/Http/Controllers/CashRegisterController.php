ae126d534bc2f138879df82ec50b8a053c1298b6
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-25 18:22:05 -0500

    test

--- app/Http/Controllers/CashRegisterController.php
+++ app/Http/Controllers/CashRegisterController.php
@@ -65,8 +65,8 @@ class CashRegisterController extends Controller
         try{
             $detailRegisters = $this->ICashRegisterRepository->getRegisterWithVoucher($id);
             if(!is_null($detailRegisters)){
-                return response()->json($detailRegisters);
-//                return new CashRegisterResource($detailRegisters);
+//                return response()->json($detailRegisters);
+                return new CashRegisterResource($detailRegisters);
             }
             else{
                 return response()->json(['messages'=>ResponseMessages::GET_RESOURCE_VOID()]);
--- app/Repositories/CashRegisterRepository.php
+++ app/Repositories/CashRegisterRepository.php
@@ -49,7 +49,7 @@ class CashRegisterRepository implements ICashRegisterRepository
     }
     public function getRegisterWithVoucher($id)
     {
-        return CashRegister::where('id', $id)->registrable;
+        return CashRegister::find($id)->registrable;
     }
 
     public function getRegistersXUser($id)
