f628f802c7f6559be32f4d7cab0bf2f0267af7da
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-25 17:44:26 -0500

    test de obtener el user

--- app/Repositories/CashRegisterRepository.php
+++ app/Repositories/CashRegisterRepository.php
@@ -49,7 +49,7 @@ class CashRegisterRepository implements ICashRegisterRepository
     }
     public function getRegisterWithVoucher($id)
     {
-        return CashRegister::find($id)->registrable;
+        return CashRegister::find($id)->with('user')->registrable;
     }
 
     public function getRegistersXUser($id)
