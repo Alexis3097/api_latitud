874f25c3872d76bfe42671ca91a9098413d620b7
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-26 15:58:20 -0500

    retornando el erro de la transaccion

--- app/Repositories/AmountAssignedRepository.php
+++ app/Repositories/AmountAssignedRepository.php
@@ -40,7 +40,7 @@ class AmountAssignedRepository implements IAmountAssignedRepository
 
         } catch (\Exception $e) {
             DB::rollback();
-            $amount = null;
+            $amount = $e;
         }
         return $amount;
     }
