9aa6fd920ddd828365fe659ce643d3b093b49863
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-26 20:15:55 -0500

    add info

--- database/seeds/UserTypeSeeder.php
+++ database/seeds/UserTypeSeeder.php
@@ -12,15 +12,19 @@ class UserTypeSeeder extends Seeder
      */
     public function run()
     {
+        //1 admin
         UserType::create([
             'type'=>'Administrador'
         ]);
+        //2 coordinador
         UserType::create([
             'type'=>'Coordinador'
         ]);
+        //3 caja chica
         UserType::create([
             'type'=>'caja_chica'
         ]);
+        //4 general
         UserType::create([
             'type'=>'General'
         ]);
