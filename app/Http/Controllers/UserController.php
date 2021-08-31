02d358676cf06f82ed57fcea8378254d110a843a
Alexis3097 <alexis.montoya97@hotmail.com>
2021-08-31 09:46:49 -0500

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,7 +96,7 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         $user = $this->IUserRepository->update($request, $id);
-        return response()->json($user);
+        return response()->json($request);
 //        try{
 //            $user = $this->IUserRepository->update($request, $id);
 //            if(!is_null($user)){
