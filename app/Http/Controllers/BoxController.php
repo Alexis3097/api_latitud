d4220f7098249ad372847fc714fb3202bcdd26e8
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-02 15:09:33 -0500

    test

--- app/Http/Controllers/BoxController.php
+++ app/Http/Controllers/BoxController.php
@@ -40,7 +40,7 @@ class BoxController extends Controller
     public function approveExpense(Request $request, $idUser){
         try{
 //            $box = $this->IBoxRepository->approveExpense($idUser, $request);
-            return response()->json([$idUser,$request->all()]);
+            return response()->json([$idUser,$request->amount]);
         }catch (Throwable $e){
             return response()->json($e);
         }
