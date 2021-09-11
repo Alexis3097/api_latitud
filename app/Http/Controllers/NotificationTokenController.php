b84c43dd074831216eb26611062a89a2fa154445
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-11 11:35:46 -0500

    fix

--- app/Http/Controllers/NotificationTokenController.php
+++ app/Http/Controllers/NotificationTokenController.php
@@ -16,15 +16,7 @@ class NotificationTokenController extends Controller
     }
 
     public function saveUserToken(StoreTest $request){
-
-//        $validated = $request->messages();
-//        return response()->json($validated);
-//        if ($validated->fails()) {
-//            return [
-//                'created' => false,
-//                'errors'  => $validated->errors()->all()
-//            ];
-//        }
+        
         $userToken = $this->INotificationTokenRepository->saveUserToken($request->user_id,$request->token);
         return response()->json($userToken);
     }
