fc1eef0ba5b1c161abb90d08df858eb795fe5424
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-11 17:21:42 -0500

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -103,7 +103,7 @@ class TestController extends Controller
     }
 
     public function  sendNoti(){
-        $response = Http::withToken( env('FCM_KEY'))->acceptJson()->post('https://fcm.googleapis.com/fcm/send',
+        $response = Http::withToken('key=AAAAnCG-6t0:APA91bHWy5x1jCMIKDXwDQras4N-lbfHkg8w9bRNnExgoMHg8WF9AzRIot-IWpvLFtyKZVM6W8KYOUKxKMtCgE8AOU3RcNAnW1b3EtqXUr-3OI182_MY9BcxtS1MXjTNiFetfw_oP5pj')->acceptJson()->post('https://fcm.googleapis.com/fcm/send',
             [
                 "notification"=>[
                     "title"=>"Titulo",
