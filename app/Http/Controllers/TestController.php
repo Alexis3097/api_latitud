c716ebab39f091d333d4c15b391dce0fa88b1a66
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-12 12:57:58 -0500

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -116,7 +116,7 @@ class TestController extends Controller
             ]);
 
 
-        return response($response->body()->multicast_id);
+        return response($response->body()->getContents());
     }
 
 
