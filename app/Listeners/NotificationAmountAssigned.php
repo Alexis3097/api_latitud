01223ce971c6b37f046f96a9e8562b7affbb14c4
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-13 16:44:18 -0500

    test

--- app/Listeners/NotificationAmountAssigned.php
+++ app/Listeners/NotificationAmountAssigned.php
@@ -28,14 +28,14 @@ class NotificationAmountAssigned
     public function handle(AmountAssignedEvent $event)
     {
         // Access the order using $event->AmountAssigned...
-        $deviceGroupRegister = DeviceGroup::where('user_id', $event->AmountAssigned->idDestinatario)->first();
+        $deviceGroupRegister = DeviceGroup::where('user_id', $event->AmountAssigned["idDestinatario"])->first();
         $response = Http::withHeaders([
             'Authorization' => env('FCM_KEY')
         ])->acceptJson()->post('https://fcm.googleapis.com/fcm/send',
             [
                 "notification"=>[
                     "title"=>"Monto asignado",
-                    "body"=>$event->AmountAssigned->remitente." te ha depositado"
+                    "body"=>$event->AmountAssigned["remitente"]." te ha depositado"
 //                    "body"=>"Ana te ha depositado"
                 ],
 //                "data"=>[
