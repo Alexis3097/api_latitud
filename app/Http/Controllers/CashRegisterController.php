a77aac10378bc0bed9017c85a97d9f1ef19cf87d
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-14 20:58:45 -0500

    test

--- app/Http/Controllers/CashRegisterController.php
+++ app/Http/Controllers/CashRegisterController.php
@@ -111,20 +111,21 @@ class CashRegisterController extends Controller
     }
     public function voidRegistration(Request $request)
     {
-//        return response()->json($request->idDestination);
-        try{
+
+//        try{
            //eliminamos el registro y nos devuelve el monto que se cancelo
             $amount = $this->ICashRegisterRepository->delete($request->idRegister);
-            //disminuimos el dinero de un usuario en concreto
-            $box = $this->IBoxRepository->approveExpense($request->idDestination, $amount);
-            if(!is_null($box)){
-                return response()->json(['messages'=>ResponseMessages::DESTROY_SUCCESS()]);
-            }else{
-                return response()->json(['messages'=>ResponseMessages::DESTROY_FAILED_400()]);
-            }
-        }catch (Throwable $e){
-            Log::info(ResponseMessages::DESTROY_FAILED_500().$e);
-            return response()->json(['voidRegistration'=>false],500);
-        }
+         return response()->json($amount);
+//            //disminuimos el dinero de un usuario en concreto
+//            $box = $this->IBoxRepository->approveExpense($request->idDestination, $amount);
+//            if(!is_null($box)){
+//                return response()->json(['messages'=>ResponseMessages::DESTROY_SUCCESS()]);
+//            }else{
+//                return response()->json(['messages'=>ResponseMessages::DESTROY_FAILED_400()]);
+//            }
+//        }catch (Throwable $e){
+//            Log::info(ResponseMessages::DESTROY_FAILED_500().$e);
+//            return response()->json(['voidRegistration'=>false],500);
+//        }
     }
 }
