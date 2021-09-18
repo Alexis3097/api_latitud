3a2993d2aad19f60691d4c6cd7e60e83606c8d89
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-18 12:31:47 -0500

    agregando softdelete a el modelo de notificaciones

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Notification extends Model
 {
     protected $table = 'notifications';
-
+    use SoftDeletes;
     protected $fillable = [
         'user_id',
         'destination_id',
