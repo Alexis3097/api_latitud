7d3d089fa748e6b4084a4c370c87dc20bd5ef52f
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-22 19:39:57 -0500

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -120,8 +120,9 @@ class TestController extends Controller
 //        }
 
         $users = User::where('user_type_id','!=',1)->get('id');
+        $notifications = Notification::where('user_id',$users[0]->id)->get();
 
-        return response()->json($users);
+        return response()->json($notifications);
 
     }
 
