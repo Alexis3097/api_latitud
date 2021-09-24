d5a7e70dc7fdb4fb14aecc739d91596c8816b588
Alexis3097 <alexis.montoya97@hotmail.com>
2021-09-24 15:54:32 -0500

    agregando notificacion de recordatorio

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,13 @@
 
 namespace App\Console;
 
+use App\Models\DeviceGroup;
+use App\Models\Notification;
+use App\Models\User;
 use App\Models\Voucher;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
@@ -50,18 +54,15 @@ class Kernel extends ConsoleKernel
         })->dailyAt('00:00');
 
         //notificaccion el ultimo dia del mes
-//        $schedule->call(function () {
-//            $vouchers = Voucher::where('approve',true)->where('photoId','!=',null)->get();
-//            if($vouchers->count() > 0){
-//                foreach ($vouchers as $voucher){
-//                    if($voucher->created_at->diffInDays() > 7){
-//                        //eliminar la foto
-//                        cloudinary()->destroy($voucher->photoId);
-//                    }
-//                }
-//
-//            }
-//        })->dailyAt('00:00');
+        // el 1 es el userType adminn
+        $schedule->call(function () {
+            $users = User::where("user_type_id", "!=", 1)->get();
+            if($users->count() > 0){
+                foreach ($users as $user){
+                    $this->sendNotification($user->id);
+                }
+            }
+        })->monthlyOn(24, '16:00');
     }
 
     /**
@@ -74,5 +75,33 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    public function sendNotification($user_id){
+        try{
+            $deviceGroupRegister = DeviceGroup::where('user_id', $user_id)->first();
+            if(!is_null($deviceGroupRegister)){
+                $response = Http::withHeaders([
+                    'Authorization' => env('FCM_KEY')
+                ])->acceptJson()->post('https://fcm.googleapis.com/fcm/send',
+                    [
+                        "notification"=>[
+                            "title"=>"Recordatorio",
+                            "body"=>"Realiza tus comprobantes de pago antes del fin de mes"
+                        ],
+                        "priority"=>"high",
+                        "to"=>$deviceGroupRegister->notification_key
+                    ]);
+                //el 1 es el id del admin
+                Notification::create([
+                    'user_id'=> 1,//a quien hizo la notificacion
+                    'destination_id'=> $user_id,//quien le pertenece la notificacion
+                    'type' => 'Recordatorio',
+                    'register_id'=>0,//es cero cuando no es registro, solo aviso
+                ]);
+            }
+        }catch (\Exception $e){
+
+        }
     }
 }
